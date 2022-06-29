16223b30d2e536ff729fb4d7cab97d45b15ea2e3
lucaslera91 <lucaslera5@gmail.com>
2022-06-28 23:59:07 -0300

    correcciones finales

--- Desafios/desafio-clase-04/index.js
+++ Desafios/desafio-clase-04/index.js
@@ -9,10 +9,9 @@ class Contenedor {
   async save(obj) {
     try {
       let archivo = await fs.promises.readFile(filePath, "utf-8");
+      let productos = JSON.parse(archivo)?.productos;
 
-      if (archivo !== "") {
-        const archivo = await fs.promises.readFile(filePath, "utf-8");
-        let productos = JSON.parse(archivo).productos;
+      if (productos.length > 0) {
         const ids = productos.map((producto) => producto.id);
         const max = Math.max(...ids);
         obj.id = max + 1;
@@ -23,7 +22,7 @@ class Contenedor {
         );
       } else {
         obj.id = 1;
-        await fs.promises.appendFile(
+        await fs.promises.writeFile(
           filePath,
           `${JSON.stringify({ productos: [obj] })}`
         );
@@ -40,12 +39,13 @@ class Contenedor {
   async getById(id) {
     try {
       const archivo = await fs.promises.readFile(filePath, "utf-8");
-
       const productos = JSON.parse(archivo).productos;
-
-      const productoById = productos.filter((item) => item.id === id);
-
-      console.log(productoById);
+      if (productos.length > 0) {
+        const productoById = productos.filter((item) => item.id === id);
+        console.log(productoById);
+      } else {
+        console.log("Producto no existe");
+      }
     } catch (err) {
       console.log(err);
     }
@@ -54,10 +54,13 @@ class Contenedor {
   async getAll() {
     try {
       const archivo = await fs.promises.readFile(filePath, "utf-8");
-
       const productos = JSON.parse(archivo).productos;
 
-      console.log(productos);
+      if (productos.length > 0) {
+        console.log(productos);
+      } else {
+        console.log("No hay productos");
+      }
     } catch (err) {
       console.log(err);
     }
@@ -66,24 +69,24 @@ class Contenedor {
   async deleteById(id) {
     try {
       const archivo = await fs.promises.readFile(filePath, "utf-8");
+      const productos = JSON.parse(archivo)?.productos;
 
-      if (archivo !== "") {
-        const productos = JSON.parse(archivo)?.productos;
-
+      if (productos.length > 0) {
         const productExists = productos.find((producto) => producto.id === id);
-
+        //console.log(productExists)
         if (productExists) {
           const finalItems = productos.filter((item) => item.id !== id);
+          console.log(finalItems);
           await fs.promises.writeFile(
             filePath,
             `${JSON.stringify({ productos: finalItems })}`
           );
           console.log(id, "deleted");
         } else {
-          console.log("Producto no existe");
+          console.log("No hay producto con ese id");
         }
       } else {
-        console.log("Archivo Vacio");
+        console.log("No hay productos existentes");
       }
     } catch (err) {
       console.log(err);
@@ -92,7 +95,7 @@ class Contenedor {
 
   deleteAll() {
     try {
-      fs.promises.writeFile(file, "", "utf-8");
+      fs.promises.writeFile(filePath, '{"productos":[]}', "utf-8");
     } catch (error) {
       console.log(error);
     }
@@ -106,13 +109,27 @@ prueba.save({
   price: "Precio",
   thumbnail: "Url Imagen",
 });
+prueba
+  .save({
+    title: "Nombre del producto",
+    price: "Precio",
+    thumbnail: "Url Imagen",
+  })
+  .then(() => {
+    prueba.getById(1);
 
-prueba.getById(2);
+    prueba.getAll();
 
-prueba.getAll();
+    prueba.deleteById(14);
+  });
 
-prueba.deleteById(1);
+//Ejecutar individualmente para ver funcionamiento
+//prueba.deleteAll()
 
-// notar que deben descomentar delete All.. prbar al final para que sea mas facil
+//los metodos se pueden ejecutar invidualmente tambien
 
-// prueba.deleteAll()
+//prueba.getById(2);
+
+//prueba.getAll();
+
+//prueba.deleteById(3);
