8233aeb32f348645b7b2ec95fd03abb4de913c2d
lucaslera91 <lucaslera5@gmail.com>
2022-07-25 19:01:12 -0300

    cambios de router

--- Desafios/desafio-ApiResful/index.js
+++ Desafios/desafio-ApiResful/index.js
@@ -1,23 +1,11 @@
 const PORT = 8080;
+const rutaProductos = require('./router/RutaProductos');
 const express = require("express");
-const { Router } = express;
-const ApiManager = require("./api");
 const app = express();
-const router = Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const filepath = "productos.txt";
 
-const api = new ApiManager(filepath);
-
-api.getProductos(router);
-api.getProducto(router);
-api.eliminarProducto(router);
-api.modificarProducto(router);
-api.agregarProducto(router);
-api.test(router);
-
-app.use('/api', router)
+app.use("/api/productos", rutaProductos);
 
 app.listen(PORT, () => {
   console.log("Listening in port: ", PORT);
