77acdd69fdfeff5164ac5c2ff5d865ed851e3c36
lucaslera91 <lucaslera5@gmail.com>
2022-07-25 19:01:20 -0300

    cambios router

--- Desafios/desafio-ApiResful/api.js
+++ Desafios/desafio-ApiResful/api.js
@@ -1,4 +1,3 @@
-const PORT = 8080;
 const fs = require("fs");
 const filepath = "productos.txt";
 
@@ -12,98 +11,85 @@ module.exports = class ApiManager {
     this.filepath = filepath;
   }
 
-  async getProductos(router) {
-    router.get("/productos", async (req, res) => {
-      const data = await getProductos();
-      return res.json({ productos: JSON.parse(data).productos });
-    });
+  async fetchProductos(req, res) {
+    const data = await getProductos();
+    return res.json({ productos: JSON.parse(data).productos });
   }
 
-  async getProducto(router) {
-    router.get("/productos/:id", async (req, res) => {
-      const data = await getProductos();
-      const productos = JSON.parse(data).productos;
-      const producto = productos.filter(
-        (producto) => Number(producto.id) === Number(req.params.id)
+  async getProducto(req, res) {
+    const data = await getProductos();
+    const productos = JSON.parse(data).productos;
+    const producto = productos.filter(
+      (producto) => Number(producto.id) === Number(req.params.id)
+    );
+    return res.json({ productos: producto });
+  }
+
+  async agregarProducto(req, res) {
+    const data = await getProductos();
+    const productos = JSON.parse(data).productos;
+    let object = req.body;
+    try {
+      const ids = productos.map((producto) => producto.id);
+      const max = Math.max(...ids);
+      object.id = max + 1;
+      productos.push(object);
+      await fs.promises.writeFile(
+        filepath,
+        `${JSON.stringify({ productos: productos })}`
       );
-      return res.json({ productos: producto });
-    });
+    } catch (error) {
+      console.log(error);
+    }
+
+    return res.json({ productos: productos });
   }
 
-  async agregarProducto(router) {
-    router.post("/productos", async (req, res) => {
-      const data = await getProductos();
-      const productos = JSON.parse(data).productos;
-      let object = req.body;
-      try {
-        const ids = productos.map((producto) => producto.id);
-        const max = Math.max(...ids);
-        object.id = max + 1;
-        productos.push(object);
-        await fs.promises.writeFile(
-          filepath,
-          `${JSON.stringify({ productos: productos })}`
-        );
-      } catch (error) {
-        console.log(error);
-      }
-
-      return res.json({ productos: productos });
-    });
-  }
-  async modificarProducto(router) {
-    router.put("/productos/:id", async (req, res) => {
-      const data = await getProductos();
-      const productos = JSON.parse(data).productos;
-      let isProducto = false;
-      const productosActualizados = productos.map((producto) => {
-        if (Number(producto.id) === Number(req.params.id)) {
-          const prodAux = {
-            title: req.body.title,
-            price: req.body.price,
-            thumbnail: req.body.thumbnail,
-            id: producto.id,
-          };
-          isProducto = true;
-          return prodAux;
-        } else {
-          return producto;
-        }
-      });
-      if (isProducto) {
-        await fs.promises.writeFile(
-          filepath,
-          `${JSON.stringify({ productos: productosActualizados })}`
-        );
-        return res.json({ productos: productosActualizados });
+  async modificarProducto(req, res) {
+    const data = await getProductos();
+    const productos = JSON.parse(data).productos;
+    let isProducto = false;
+    const productosActualizados = productos.map((producto) => {
+      if (Number(producto.id) === Number(req.params.id)) {
+        const prodAux = {
+          title: req.body.title,
+          price: req.body.price,
+          thumbnail: req.body.thumbnail,
+          id: producto.id,
+        };
+        isProducto = true;
+        return prodAux;
       } else {
-        return res.json({ msg: "Producto no existe" });
+        return producto;
       }
     });
-  }
-  async eliminarProducto(router) {
-    router.delete("/productos/:id", async (req, res) => {
-      const data = await getProductos();
-      const productos = JSON.parse(data).productos;
-      const listaActualizada = productos.filter(
-        (producto) => Number(producto.id) !== Number(req.params.id)
+    if (isProducto) {
+      await fs.promises.writeFile(
+        filepath,
+        `${JSON.stringify({ productos: productosActualizados })}`
       );
+      return res.json({ productos: productosActualizados });
+    } else {
+      return res.json({ msg: "Producto no existe" });
+    }
+  }
+  async eliminarProducto(req, res) {
+    const data = await getProductos();
+    const productos = JSON.parse(data).productos;
+    const listaActualizada = productos.filter(
+      (producto) => Number(producto.id) !== Number(req.params.id)
+    );
 
-      if (listaActualizada.length < productos.length) {
-        await fs.promises.writeFile(
-          filepath,
-          `${JSON.stringify({ productos: listaActualizada })}`
-        );
-        return res.json({ productos: listaActualizada });
-      } else {
-        return res.json({ msg: "Producto no existe" });
-      }
-    });
+    if (listaActualizada.length < productos.length) {
+      await fs.promises.writeFile(
+        filepath,
+        `${JSON.stringify({ productos: listaActualizada })}`
+      );
+      return res.json({ productos: listaActualizada });
+    } else {
+      return res.json({ msg: "Producto no existe" });
+    }
   }
-  async test(router){
-      router.post('/test' , async (req, res) => {
-          return res.json( req.body );
-      })
-  }
-}
+};
+
 
